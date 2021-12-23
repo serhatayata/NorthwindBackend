a358eaef369c389f183d28331a89555760cc9db2
Serhat Ayata <srhtayata@gmail.com>
2021-12-23 12:05:27 +0300

    AuthController

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,12 @@ namespace Business.Constants
         public static string ProductAdded = "Product has been added successfully.";
         public static string ProductDeleted = "Product has been deleted successfully.";
         public static string ProductUpdated = "Product has been updated successfully.";
-
+        public static string UserNotFound = "User hasn't been found.";
+        public static string PasswordError = "Password error!";
+        public static string SuccessfullLogin = "Login is successful.";
+        public static string UserAlreadyExists = "User already exists.";
+        public static string UserRegistered = "User register is successful.";
+        public static string AccessTokenCreated = "Access Token is created successfully.";
+   
     }
 }
